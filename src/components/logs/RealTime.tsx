b9f9b7d20c1d754cac4d22235c5d7b68576c5ba7
hiro2029 <hiro.2029@icloud.com>
2023-09-28 19:58:13 +0900

    fix: Heatmap

--- src/components/logs/RealTime.tsx
+++ src/components/logs/RealTime.tsx
@@ -5,7 +5,16 @@ import { useFetchInfomationCardLogs } from "../../api/logs/Logs";
 import Loading from "../../pages/loading/Loading";
 import { Box } from "@mui/material";
 import { Circle } from "@mui/icons-material";
-
+// getColor関数を定義
+function getColor(trial: number) {
+  // trialを0から1に標準化し、30で割る（30を超えている場合でも30で割る）
+  const normalizedValue = Math.min(trial / 30, 1);
+  // 色を計算
+  const red = 255;
+  const green = Math.round(255 * (1 - normalizedValue));
+  const blue = Math.round(255 * (1 - normalizedValue));
+  return `rgb(${red}, ${green}, ${blue})`;
+}
 export const RealTime = () => {
   const [data, setData] = useState([]);
   const postQuery: UseQueryResult<CardLog[], unknown> =
@@ -38,59 +47,52 @@ export const RealTime = () => {
                 justifyContent: "center", // 子要素を中央に配置
               }}
             >
-              <div className="circle">
-                {user.info[user.info.length - 1].card_detail_logs[
-                  user.info[user.info.length - 1].card_detail_logs.length - 1
-                ] ? (
-                  100 *
-                    user.info[user.info.length - 1].card_detail_logs[
-                      user.info[user.info.length - 1].card_detail_logs.length -
-                        1
-                    ].levenshtein_distance !==
-                  0 ? (
+              <div className="circle-outer">
+                <div className="circle">
+                  {user.info[user.info.length - 1].card_detail_logs[
+                    user.info[user.info.length - 1].card_detail_logs.length - 1
+                  ] ? (
+                    100 *
+                      user.info[user.info.length - 1].card_detail_logs[
+                        user.info[user.info.length - 1].card_detail_logs
+                          .length - 1
+                      ].levenshtein_distance !==
+                    0 ? (
+                      <Circle
+                        style={{
+                          width: `${
+                            100 *
+                            user.info[user.info.length - 1].card_detail_logs[
+                              user.info[user.info.length - 1].card_detail_logs
+                                .length - 1
+                            ].levenshtein_distance
+                          }px`,
+                          height: `${
+                            100 *
+                            user.info[user.info.length - 1].card_detail_logs[
+                              user.info[user.info.length - 1].card_detail_logs
+                                .length - 1
+                            ].levenshtein_distance
+                          }px`,
+                          color: getColor(
+                            user.info[user.info.length - 1].card_detail_logs[
+                              user.info[user.info.length - 1].card_detail_logs
+                                .length - 1
+                            ].trial
+                          ),
+                        }}
+                      />
+                    ) : null
+                  ) : (
                     <Circle
                       style={{
-                        width: `${
-                          100 *
-                          user.info[user.info.length - 1].card_detail_logs[
-                            user.info[user.info.length - 1].card_detail_logs
-                              .length - 1
-                          ].levenshtein_distance
-                        }px`,
-                        height: `${
-                          100 *
-                          user.info[user.info.length - 1].card_detail_logs[
-                            user.info[user.info.length - 1].card_detail_logs
-                              .length - 1
-                          ].levenshtein_distance
-                        }px`,
-                        color: `rgb(255, ${Math.round(
-                          255 *
-                            (1 -
-                              user.info[user.info.length - 1].card_detail_logs[
-                                user.info[user.info.length - 1].card_detail_logs
-                                  .length - 1
-                              ].levenshtein_distance)
-                        )}, ${Math.round(
-                          255 *
-                            (1 -
-                              user.info[user.info.length - 1].card_detail_logs[
-                                user.info[user.info.length - 1].card_detail_logs
-                                  .length - 1
-                              ].levenshtein_distance)
-                        )}`,
+                        width: "100px",
+                        height: "100px",
+                        color: `rgb(255,230,230)`,
                       }}
                     />
-                  ) : null
-                ) : (
-                  <Circle
-                    style={{
-                      width: "100px",
-                      height: "100px",
-                      color: "red",
-                    }}
-                  />
-                )}
+                  )}
+                </div>
               </div>
               <div className="user" style={{ color: "white" }}>
                 <p>{`user${index + 1}`}</p>
