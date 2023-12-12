743ef9958b93dfc83b103447cc9ff5c5d6d883df
hiro2029 <hiro.2029@icloud.com>
2023-12-12 18:38:57 +0900

    feat: 振り返り機能

---
+++ src/components/logs/Review.tsx
@@ -0,0 +1,120 @@
+import { UseQueryResult } from "@tanstack/react-query";
+import React, { useEffect, useState } from "react";
+import { CardLog } from "../../types/log";
+import { useFetchInfomationCardLogs } from "../../api/logs/Logs";
+import Loading from "../../pages/loading/Loading";
+import { Box } from "@mui/material";
+import { Circle } from "@mui/icons-material";
+// getColor関数を定義
+function getColor(trial: number) {
+  // trialを0から1に標準化し、30で割る（30を超えている場合でも30で割る）
+  const normalizedValue = Math.min(trial / 30, 1);
+  // 色を計算
+  const red = 255;
+  const green = Math.round(255 * (1 - normalizedValue));
+  const blue = Math.round(255 * (1 - normalizedValue));
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+export const Review = () => {
+  const [data, setData] = useState([]);
+  const postQuery: UseQueryResult<CardLog[], unknown> =
+    useFetchInfomationCardLogs();
+  // console.log(postQuery);
+
+  // 指定された日時をDateオブジェクトとして取得する
+  const specifiedTime = new Date("2023-09-28T22:10:21.560Z").getTime();
+
+  const [currentTime, setCurrentTime] = useState(specifiedTime); // 指定された時間を状態として保持する
+
+  // // 現在の時間を更新する関数
+  const updateCurrentTime = () => {
+    setCurrentTime((prevTime) => prevTime + 1000); // 1秒ごとに時間を更新する
+  };
+
+  useEffect(() => {
+    // 1秒ごとに時間を更新するための setInterval を設定する
+    const interval = setInterval(() => {
+      updateCurrentTime();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+  // console.log(currentTime);
+
+  const filteredData =
+    postQuery.data?.map((user) => {
+      const filteredLogs = user.info[
+        user.info.length - 1
+      ].card_detail_logs.filter((log) => {
+        const logTime = new Date(log.answer_at).getTime(); // エポック時間（ミリ秒単位の時間）として取得
+        return logTime <= currentTime; // 指定された時間までのデータをフィルタリングする
+      });
+
+      return {
+        ...user,
+        info: [
+          {
+            ...user.info[user.info.length - 1],
+            card_detail_logs: filteredLogs, // 指定された時間までのデータにフィルタリングされたlogsを代入する
+          },
+        ],
+      };
+    }) || [];
+  console.log(filteredData);
+
+  return (
+    <div className="logs">
+      <p>Current Time: {currentTime}</p>
+      <h1>Realtime Log Data</h1>
+      <div className="realtime">
+        {filteredData?.map((user, index) => (
+          <div className="box">
+            <Box
+              key={user.user_id}
+              style={{
+                display: "flex",
+                flexDirection: "column", // 子要素を縦方向に配置
+                alignItems: "center", // 子要素を中央に配置
+                justifyContent: "center", // 子要素を中央に配置
+              }}
+            >
+              <div className="circle-outer">
+                <div className="circle">
+                  {user.info[user.info.length - 1].card_detail_logs[
+                    user.info[user.info.length - 1].card_detail_logs.length - 1
+                  ] ? (
+                    <Circle
+                      style={{
+                        width: `100px`,
+                        height: `100px`,
+                        color: getColor(
+                          user.info[user.info.length - 1].card_detail_logs[
+                            user.info[user.info.length - 1].card_detail_logs
+                              .length - 1
+                          ].trial
+                        ),
+                      }}
+                    />
+                  ) : (
+                    <Circle
+                      style={{
+                        width: "100px",
+                        height: "100px",
+                        color: `rgb(255,230,230)`,
+                      }}
+                    />
+                  )}
+                </div>
+              </div>
+              <div className="user" style={{ color: "white" }}>
+                <p>{`user${index + 1}`}</p>
+              </div>
+            </Box>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// export default RealTime;
--- src/pages/logs/Logs.tsx
+++ src/pages/logs/Logs.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { Circle } from "@mui/icons-material";
 import { LogsTable } from "../../components/logs/LogsTable";
 import { RealTime } from "../../components/logs/RealTime";
+import { Review } from "../../components/logs/Review";
 interface TabPanelProps {
   children?: React.ReactNode;
   index: number;
@@ -53,6 +54,7 @@ const Logs = () => {
         >
           <Tab label="Log" {...a11yProps(0)} />
           <Tab label="Real-Time-Monitaling" {...a11yProps(1)} />
+          <Tab label="review" {...a11yProps(1)} />
         </Tabs>
       </Box>
       <CustomTabPanel value={value} index={0}>
@@ -60,6 +62,9 @@ const Logs = () => {
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         <RealTime />
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        <Review />
       </CustomTabPanel>
     </div>
   );
