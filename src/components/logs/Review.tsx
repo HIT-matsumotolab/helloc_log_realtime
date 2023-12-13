e96385f30668988f3abfbd0a285175d5bbeb93e2
hiro2029 <hiro.2029@icloud.com>
2023-12-13 16:56:30 +0900

    fix

--- src/components/logs/Review.tsx
+++ src/components/logs/Review.tsx
@@ -23,12 +23,18 @@ export const Review = () => {
 
   // 指定された日時をDateオブジェクトとして取得する
   const specifiedTime = new Date("2023-09-28T22:10:21.560Z").getTime();
+  const time = 60;
+  const afterTime = specifiedTime + time * 1000;
 
   const [currentTime, setCurrentTime] = useState(specifiedTime); // 指定された時間を状態として保持する
+  // const [time, setTime] = useState(0);
 
   // // 現在の時間を更新する関数
   const updateCurrentTime = () => {
     setCurrentTime((prevTime) => prevTime + 1000); // 1秒ごとに時間を更新する
+    // if (time < 60) {
+    //   setTime((prevtime) => prevtime + 1);
+    // }
   };
 
   useEffect(() => {
@@ -41,13 +47,36 @@ export const Review = () => {
   }, []);
   // console.log(currentTime);
 
+  // beforeTimeを1分前のエポック時間（ミリ秒単位）に設定する
+  const beforeTime = currentTime - 60 * 1000;
+
   const filteredData =
     postQuery.data?.map((user) => {
       const filteredLogs = user.info[
         user.info.length - 1
       ].card_detail_logs.filter((log) => {
         const logTime = new Date(log.answer_at).getTime(); // エポック時間（ミリ秒単位の時間）として取得
-        return logTime <= currentTime; // 指定された時間までのデータをフィルタリングする
+        return logTime <= currentTime && logTime > beforeTime; // 1分前のデータから現在のデータまでのフィルタリング
+      });
+
+      const beforeTimeLogs = user.info[
+        user.info.length - 1
+      ].card_detail_logs.filter((log) => {
+        const logTime = new Date(log.answer_at).getTime(); // エポック時間（ミリ秒単位の時間）として取得
+        return logTime <= beforeTime; // 1分前のデータをフィルタリング
+      });
+
+      // beforeTimeLogsとfilteredLogsのlog.trialの差分を求める
+      const diffLogs = filteredLogs.map((log) => {
+        const trialBefore =
+          beforeTimeLogs.find(
+            (beforeLog) =>
+              beforeLog.card_detail_log_id === log.card_detail_log_id
+          )?.trial ?? 0;
+        return {
+          ...log,
+          trialDiff: log.trial - trialBefore, // log.trialの差分を計算
+        };
       });
 
       return {
@@ -55,11 +84,12 @@ export const Review = () => {
         info: [
           {
             ...user.info[user.info.length - 1],
-            card_detail_logs: filteredLogs, // 指定された時間までのデータにフィルタリングされたlogsを代入する
+            card_detail_logs: diffLogs, // 差分データを代入
           },
         ],
       };
     }) || [];
+
   console.log(filteredData);
 
   return (
@@ -85,22 +115,29 @@ export const Review = () => {
                   ] ? (
                     <Circle
                       style={{
-                        width: `100px`,
-                        height: `100px`,
-                        color: getColor(
+                        width: `${
+                          10 *
                           user.info[user.info.length - 1].card_detail_logs[
                             user.info[user.info.length - 1].card_detail_logs
                               .length - 1
                           ].trial
-                        ),
+                        }px`,
+                        height: `${
+                          10 *
+                          user.info[user.info.length - 1].card_detail_logs[
+                            user.info[user.info.length - 1].card_detail_logs
+                              .length - 1
+                          ].trial
+                        }px`,
+                        color: `rgb(255,0,0)`,
                       }}
                     />
                   ) : (
                     <Circle
                       style={{
-                        width: "100px",
-                        height: "100px",
-                        color: `rgb(255,230,230)`,
+                        width: "0px",
+                        height: "0px",
+                        color: `rgb(255,0,0)`,
                       }}
                     />
                   )}
